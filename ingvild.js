0092cf620f4d82005518009326c391ac234f0cb9
Ingvild <ingvild.sandstad@gmail.com>
2021-03-08 14:14:23 +0100

    nytt i CSS og ingvild.js

--- ingvild.js
+++ ingvild.js
@@ -1,1 +1,30 @@
+this.snake = {
+    position: 44,
+    direction: 'E'
+}
 
+setInterval(function(){
+    move();
+}, 5000);
+
+
+function move() {
+    console.log(snake.position)
+    console.log(this.snake.position);
+    let id = `box${snake.position}`;
+}
+
+
+
+function positionChange(snake) {
+    switch (snake.direction) {
+        case 'N':
+            return - 10;
+        case 'S':
+            return + 10;
+        case 'E':
+            return + 1;
+        case 'W':
+            return + 1;   
+    }
+}
